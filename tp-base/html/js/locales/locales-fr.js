26564ae489c0ddfb98fdd28d34eda3ffc7392142
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2022-07-06 12:21:24 +0300

    Add files via upload

---
+++ tp-base/html/js/locales/locales-fr.js
@@ -0,0 +1,67 @@
+var Locales = new Object();
+
+// Siderbar Locales.
+Locales.sideBarInventory = "Inventaire";
+Locales.sidebarPersonalProfile = "Profil personnel";
+Locales.sidebarScoreboard = "Liste des joueurs en ligne";
+Locales.sidebarTickets = "Créer un billet";
+
+// Inventory Locales.
+Locales.inventoryGiveItem = "DONNER";
+Locales.inventoryDropItem = "GOUTTE";
+
+// Profile Locales
+Locales.profileTitle = "PROFIL";
+Locales.profileCharacterStatistics = "Statistiques de personnage";
+Locales.profileStatisticsOverview = "Aperçu des statistiques";
+Locales.profileHoldingCash = "DÉTENIR DE L'ARGENT";
+Locales.profileBankAccount = "COMPTE EN BANQUE";
+Locales.profileBlackMoney = "ARGENT NOIR";
+
+Locales.profileChangeUrl = "Modifier l'URL de l'avatar";
+
+// Scoreboard Locales.
+Locales.scoreboardOnlinePlayers = "Joueurs en ligne: ";
+Locales.scoreboardSteamName = "Steam Name";
+Locales.scoreboardUserName = "Nom d'utilisateur";
+Locales.scoreboardId = "ID";
+Locales.scoreboardMS = "MS (Ping)";
+
+// Tickets Locales.
+Locales.ticketsTitle = "CRÉER DES BILLETS D'ASSISTANCE";
+Locales.ticketsDescription = "La description";
+Locales.ticketsSubmit = "NOUS FAIRE PARVENIR";
+Locales.ticketsShowTickets = "Afficher les billets disponibles";
+
+Locales.ticketsNoDescription = "There is no description.";
+Locales.ticketsTeleport = "TÉLÉPORTER";
+Locales.ticketsSetSolved = "ENSEMBLE RÉSOLU";
+Locales.ticketsRefreshTickets = "Actualiser les billets";
+
+// Feedback Locales
+
+Locales.feedbackTitle = "COMMENT POUVONS-NOUS AMÉLIORER NOTRE SERVEUR POUR VOUS?";
+Locales.feedbackFirstSubject = "Sujet suggéré";
+Locales.feedbackSecondSubject = "Décrivez votre proposition";
+Locales.feedbackSendButton = "Envoyer";
+
+// Info Commands & Keybinds Locales (Use: \n to create new line for informationCommands & informationKeybinds Locales).
+
+Locales.informationCommandsOption = "> Commandes de jeu";
+Locales.informationKeybindsOption = "> Raccourcis clavier du jeu";
+
+Locales.informationCommands = "• /openbase - Interface utilisateur Open Base."
++ "\n "
++ "\n • /me - permet d'afficher une action spécifique au-dessus de la tête d'un joueur. Il est particulièrement utilisé pour des actions qui n'auraient pas pu être faites en jeu dans un contexte de jeu de rôle. "
++ "\n "
++ "\n• /postal - Il montre votre adresse postale actuelle (emplacement).";
+
+Locales.informationKeybinds = "• T - Pour ouvrir le chat ou L (lorsque le chat est désactivé) pour changer le type d'affichage du chat."
++ "\n "
++ "\n• ESC - Pour ouvrir le menu principal."
++ "\n "
++ "\n• N - Pour utiliser le mode chat vocal.";
+
+// Trunk Inventory Locales
+
+Locales.trunkCurrentWeight = "Poids du coffre: ";
